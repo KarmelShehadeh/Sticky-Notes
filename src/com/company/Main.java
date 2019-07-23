ab3439f9f08919603c5a409c1ee65cf9d2586d61
karmel shehadeh <karmel.1195@gmail.com>
2019-07-24 00:57:53 +0300

    view all user’s notes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 import java.io.File;
 
@@ -8,12 +11,14 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
         //create the menu here
      //   addNewUser();
         viewNotes();
 
     }
     private static void addNewUser() throws IOException{
+        String path = "C:/Users/karme/OneDrive/Desktop/Notes/";
 
         Scanner input = new Scanner(System.in);
         System.out.print("Welcome aboard new user!\n ");
@@ -28,22 +33,53 @@ public class Main {
         System.out.print("Done! ");
         System.out.print("\nNice to meet you " +firstName+" "+ lastName);
         String fileName = firstName + lastName + ".txt";
-        File file = new File("C:/Users/karme/OneDrive/Desktop/Nots/"+fileName);// creat the file in the written path
+        File file = new File(path +fileName);// create the file in the written path
         System.out.print("\n<Click Enter to return to main menu>");
         boolean isFileCreated = file.createNewFile();
-        //System.out.print("Was the file created? -- ");
-        //System.out.println(isFileCreated);
+        if(isFileCreated)
+        System.out.print("The file Created Successfully");
+        else
+            System.out.print("There is a Problem");
 
     }
-    private static void viewNotes() throws IOException{
-        File f = new File("C:/Users/karme/OneDrive/Desktop/Nots/");
-        File[] matchingFiles = f.listFiles(new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                return name.startsWith("temp") && name.endsWith("txt");
+
+    private static void viewNotes() throws IOException {
+        String path = "C:/Users/karme/OneDrive/Desktop/Notes/"; //the path of notes directory
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Retrieve your notes? Absolutely! \n" +
+                "Please let know your full name first: \n");
+
+        String yourName = input.nextLine(); //the user have to write his name here
+        String fullName = yourName + ".txt"; //add .txt extintion to the user name
+
+        File dir = new File(path); //create dir object
+        FilenameFilter filter = new FilenameFilter() {
+            public boolean accept (File dir, String name) {
+                return name.startsWith(fullName);
             }
-        });
+        };
+
+        String[] children = dir.list(filter); //create children list
+        if (children == null) {       //if there is no directory with the previous path
+
+            System.out.println("Either dir does not exist or is not a directory");
+        } else {
+            if(children.length==0)//if there is no  file with the written name
+                System.out.print("Umm, can’t find any saved notes for you.");
+         else{ // if the file exist
+
+                    System.out.println("Found it!\n" +
+                            "Here are your stored notes:\n ");
+                    Files.lines(Paths.get(path + fullName ), StandardCharsets.UTF_8).forEach(System.out::println); //print the content of the file
+
+                    System.out.print("\n Happy to serve you :)");
+                }
+                }
 
 
+        }
 
     }
-}
+
+
